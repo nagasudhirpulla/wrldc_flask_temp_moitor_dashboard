0142560b549b683bd29e52e8b57125427e7c9ba6
nagasudhirpulla <nagasudhirpulla@gmail.com>
2020-09-09 15:14:14 +0530

    minor changes

--- setup_service.bat
+++ setup_service.bat
@@ -1,4 +1,4 @@
 call nssm.exe install wrldc_tmp_hum_monitor "%cd%\run_server.bat"
-call nssm.exe set wrldc_tmp_hum_monitor AppStdout "%cd%\logs\mis_rpc_gen_hrs.log"
-call nssm.exe set wrldc_tmp_hum_monitor AppStderr "%cd%\logs\mis_rpc_gen_hrs.log"
+call nssm.exe set wrldc_tmp_hum_monitor AppStdout "%cd%\logs\wrldc_tmp_hum_monitor.log"
+call nssm.exe set wrldc_tmp_hum_monitor AppStderr "%cd%\logs\wrldc_tmp_hum_monitor.log"
 call sc start wrldc_tmp_hum_monitor
